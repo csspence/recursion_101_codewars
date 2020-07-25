10c3fc7081de6fd8a81670e0c40e1a727071bf5a
Crew Spence <csspence123@gmail.com>
2020-07-25 08:38:10 -0500

    Wrote working solution.

--- recursion_101_codewars.js
+++ recursion_101_codewars.js
@@ -6,6 +6,16 @@ ii) If a ≥ 2*b, set a = a - 2*b, and repeat step (i). Otherwise, go to
 iii) If b ≥ 2*a, set b = b - 2*a, and repeat step (i). Otherwise, return [a,b].
 */
 
-function solve(a,b){
-    //..
+const solve = (a,b) => {
+  if(a === 0 || b === 0) {
+    return [a, b];
+  } else if(a >= 2 * b) {
+    a = a - 2 * b;
+    return solve(a, b);
+  } else if(b >= 2 * a) {
+    b = b - 2 * a;
+    return solve(a, b);
+  } else {
+      return [a, b];
+  }
 }
